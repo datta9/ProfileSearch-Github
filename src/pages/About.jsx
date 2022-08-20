f94dc843bbbdedbaefe23d7f46cf94ae73808936
datta9 <datta.yh@gmail.com>
2022-08-20 22:41:53 +0530

    All files added

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+    return (
+        <div>
+            <h1 className='text-6xl mb-4 text-white'>Github Finder</h1>
+            <p className='mb-4 text-2xl font-light text-white'>
+                A React app to search GitHub profiles and see profile details. This project is a part of learning React concepts by Phani.
+            </p>
+            <p className='text-lg text-gray-400'>
+                Version <span className='text-white'>1.0.0</span>
+            </p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import UserResults from "../components/users/UserResults";
+import UserSearch from "../components/users/UserSearch";
+
+function Home() {
+
+    return (
+        <React.Fragment>
+            <UserSearch />
+            <UserResults />
+        </React.Fragment>
+    );
+}
+
+export default Home;
