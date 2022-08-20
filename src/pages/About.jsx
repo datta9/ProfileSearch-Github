5012053d70f32929c88c7d7b2d585e90995e5bfb
datta9 <datta.yh@gmail.com>
2022-08-20 22:49:24 +0530

    About page content modified

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ function About() {
         <div>
             <h1 className='text-6xl mb-4 text-white'>Github Finder</h1>
             <p className='mb-4 text-2xl font-light text-white'>
-                A React app to search GitHub profiles and see profile details. This project is a part of learning React concepts by Phani.
+                A React app to search GitHub profiles and see profile details. This project is a part of learning React concepts by Phani Datta.
             </p>
             <p className='text-lg text-gray-400'>
                 Version <span className='text-white'>1.0.0</span>
