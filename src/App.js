106dfa6a1144e32b1d4e366d2d8c95bb6ab9a5ea
datta9 <datta.yh@gmail.com>
2022-08-21 19:05:38 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NotFound from './pages/NotFound';
 import { GithubProvider } from './context/github/GithubContext.js';
 import { AlertProvider } from './context/alert/AlertContext';
 import Alert from './components/Layout/Alert';
+import User from './pages/User';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
               <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
+                <Route path='/user/:login' element={<User />} />
                 <Route path='/notFound' element={<NotFound />} />
                 <Route path='/*' element={<NotFound />} />
               </Routes>
